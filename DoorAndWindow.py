4db8087cd18400d1f6c73c6daba60b971c0018ca
PPTide <52575064+PPTide@users.noreply.github.com>
2023-11-22 15:59:57 +0000

    stuff

--- DoorAndWindow.py
+++ DoorAndWindow.py
@@ -1,18 +1,25 @@
 import FurnitureTurtle as ft
+import turtle
 
 
 class DoorAndWindowTurtle(ft.FurnitureTurtle):
     def move(self, x, y):
         if x < 180 and x > -530 and y < 150:
-            y = -147.5
+            y = -145
             self.rotation = 90
         elif x < 180 and x > -530 and y > 150:
-            y = 297.5
+            y = 295
             self.rotation = 270
-        elif x < -530 and y > -205 and y < 215:
-            x = -612.5
+        elif x < -530 and y > -105 and y < 255:
+            x = -570
             self.rotation = 0
-        elif x > 180 and y > -205 and y < 215:
-            x = 182.5
+        elif x > 180 and y > -105 and y < 255:
+            x = 220
             self.rotation = 180
+        else:
+          turtle.TK.messagebox.showerror(
+              title="Out of bounds",
+              message="Das Objekt wurde außerhalb des Raums platziert.",
+          )
+          return
         super().move(x, y)
